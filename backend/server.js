6fb68110e78e38825ee2879d2f27b5c2b93f2e1f
Hemant Kumar <hemant.k2807@gmail.com>
2026-05-18 13:28:33 +0530

    backend deployed on render

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const generatePdf = require("./services/generatePdf");
 const sendEmail = require("./services/sendEmail");
 const logToSheets = require("./services/logToSheets");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json({ limit: "1mb" }));
 
 app.use((req, _res, next) => {
